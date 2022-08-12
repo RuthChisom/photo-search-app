c7cedface1745081cf4e33e0619dc030fe30a48d
Ruth Chisom <ndukwe100805043@gmail.com>
2022-08-12 18:26:58 +0100

    display full image in modal when clicked

--- app.js
+++ app.js
@@ -46,12 +46,38 @@ app.controller('FlickrCtrl', function($scope, $http, Flickr) {
     });
 
   }
-  
+
   // reset form to initial state
   $scope.resetForm = function (form) {
     $scope.form.tags.$setValidity();
     $scope.images = {};
     $scope.searchCriteria = {};
+  };
+
+  // open image in full
+  $scope.OpenImg = function(image){
+    var modal = document.getElementById("myModal");
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    // var img = document.getElementById("myImg");
+    var modalImg = document.getElementById("modlImg");
+    var captionText = document.getElementById("caption");
+      modal.style.display = "block";
+      modalImg.src = image.media.m;
+      // construct details
+      captionText.innerHTML = image.title+ " Published by "+ image.author + " On " + image.date_taken;
+  } 
+
+  //close image popup
+  $scope.closeHeaderPopup = function () {
+    var modal = document.getElementById("myModal");
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() { 
+      modal.style.display = "none";
+    }
   };
 
   // Flickr.get();

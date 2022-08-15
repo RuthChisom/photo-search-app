b235eac290d9e08aa9c04b71f46ffce35499121e
Ruth Chisom <ndukwe100805043@gmail.com>
2022-08-15 14:32:06 +0100

    clean up code

--- app.js
+++ app.js
@@ -1,22 +1,6 @@
 var app = angular.module('flickrApp', ['ngResource']);
 
-app.factory('Flickr', function($resource, $q) {
-  var photosPublic = $resource('https://api.flickr.com/services/feeds/photos_public.gne?tags=trees&format=json', 
-      { format: 'json', jsoncallback: 'JSON_CALLBACK' }, 
-      { 'load': { 'method': 'JSONP' } });
-  
-  return {
-    get: function() {
-      var q = $q.defer();
-      photosPublic.load(function(resp) {
-        q.resolve(resp);
-        console.log(resp.items);
-      })
-      return q.promise;
-    }
-  }
-});
-app.controller('FlickrCtrl', function($scope, $http, Flickr) {
+app.controller('FlickrCtrl', function($scope, $http) {
   // $scope.master = {};
   // $scope.images = {};
   $scope.search = function (searchCriteria) {
@@ -31,7 +15,8 @@ app.controller('FlickrCtrl', function($scope, $http, Flickr) {
     flickrAPI = flickrAPI + "?jsoncallback=JSON_CALLBACK"
           + "&tags=" + encodeURIComponent($scope.searchCriteria.tags)
           + "&format=json";
-    // send AJAX query to Flickr API
+
+    // send request to Flickr API
     $http.jsonp(flickrAPI)
     .success(function (data, status, headers, config) {
         console.log(data,status);
@@ -58,14 +43,13 @@ app.controller('FlickrCtrl', function($scope, $http, Flickr) {
   $scope.OpenImg = function(image){
     var modal = document.getElementById("myModal");
 
-    // Get the image and insert it inside the modal - use its "alt" text as a caption
-    // var img = document.getElementById("myImg");
+    // Get the image and insert it inside the modal
     var modalImg = document.getElementById("modlImg");
     var captionText = document.getElementById("caption");
       modal.style.display = "block";
       modalImg.src = image.media.m;
       // construct details
-      captionText.innerHTML = image.title+ " Published by "+ image.author + " On " + image.date_taken;
+      // captionText.innerHTML = image.title+ " Published by "+ image.author + " On " + image.date_taken;
   } 
 
   //close image popup
@@ -75,11 +59,6 @@ app.controller('FlickrCtrl', function($scope, $http, Flickr) {
     var span = document.getElementsByClassName("close")[0];
 
     // When the user clicks on <span> (x), close the modal
-    span.onclick = function() { 
       modal.style.display = "none";
-    }
   };
-
-  // Flickr.get();
-  
 });
